60652da75a8c1e293b76a54f730117d44ef294f3
sainath-07 <sainathb308@gmail.com>
2024-09-15 15:59:06 +0530

    save todo

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,11 +2,23 @@ import todoReducer from "./reducer";
 import { legacy_createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
 
-// Create store and apply middleware
 const todoStore = legacy_createStore(
   todoReducer, // Your root reducer
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+
+
+
+
+// // Instead of legacy_createStore, use configureStore:
+// const todoStore = configureStore({
+//   reducer: {
+//     todo: todoReducer,  // Provide your reducer(s) here
+//   },
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+// });
 
 export default todoStore;
